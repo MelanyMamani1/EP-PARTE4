716ded3e2593ed5e91a19d48c949b17b8e5fbf51
hans <2123110276@untels.edu.pe>
2024-05-20 15:59:28 +0200

    partre 3 herrera

--- aplicacion.py
+++ aplicacion.py
@@ -1,33 +1,77 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon May 20 15:41:34 2024
-
-@author: juger
-"""
-
-def validar_credenciales(usuario, clave):
-    with open("usuario.txt", "r") as file_usuario, open("claves.txt", "r") as file_claves:
-        logins = file_usuario.read().splitlines()
-        claves = file_claves.read().splitlines()
-        # Verificar si el usuario y la clave coinciden
-        if usuario in logins and clave == claves[logins.index(usuario)]:
+def validar_credenciales(login, clave):
+    with open("usuario.txt", "r") as file_login, open("claves.txt", "r") as file_clave:
+        logins = file_login.read().splitlines()
+        claves = file_clave.read().splitlines()
+        if login in logins and clave in claves:
             return True
         else:
             return False
 
+def listar_personas():
+    try:
+        with open("codigo.txt", "r") as file_codigo, open("nombre.txt", "r") as file_nombre, open("precio.txt", "r") as file_precio:
+            codigos = file_codigo.read().splitlines()
+            nombres = file_nombre.read().splitlines()
+            precios = file_precio.read().splitlines()
+            if len(codigos) == len(nombres) == len(precios):
+                print("Datos de las personas:")
+                print("{:<10} {:<20} {:<10}".format("CODIGO", "NOMBRE", "PRECIO"))
+                for codigo, nombre, precio in zip(codigos, nombres, precios):
+                    print("{:<10} {:<20} {:<10}".format(codigo, nombre, precio))
+            else:
+                print("Error: Los archivos de datos no tienen la misma cantidad de registros.")
+    except FileNotFoundError:
+        print("Error: No se encontraron los archivos necesarios.")
+
+
+def agregar_persona(codigos, nombre, precio):
+    try:
+        # Si el nombre no está presente, agregar la nueva persona
+        with open("codigo.txt", "a") as file_codigo, open("nombre.txt", "a") as file_nombre, open("precio.txt", "a") as file_precio:
+            file_codigo.write(codigos +"\n")
+            file_nombre.write(nombre +"\n")
+            file_precio.write(precio +"\n")
+        print("Producto agregado exitosamente.")
+    except IOError:
+        print("Error al escribir en los archivos.")
+
+
 intentos = 0
 while intentos < 2:
-    usuario = input("Ingrese su login: ")
+    login = input("Ingrese su login: ")
     clave = input("Ingrese su clave: ")
-    if validar_credenciales(usuario, clave):
-        print("DATOS PRODUCTO")
-        print("Menú:")
-        print("1. Listar")
-        print("2. Agregar")
-        print("3. Salir")
-        break
+    if validar_credenciales(login, clave):
+        print("Bienvenido al programa.")
+        while True:
+            try:
+                print("Menú:")
+                print("1. Listar productos")
+                print("2. Agregar productos")
+                print("3. Salir")
+                opcion = input("Seleccione una opción: ")
+                if opcion == "1":
+                    listar_personas()
+                elif opcion == "2":
+                    codigos = input("Ingrese el codigo: ")
+                    nombre = input("Ingrese el nombre: ")
+                    precio = input("Ingrese el precio: ")
+                    agregar_persona(codigos, nombre, precio)
+                elif opcion == "3":
+                    print("Saliendo del programa.")
+                    break
+                else:
+                    print("Opción inválida. Intente nuevamente.")
+            except KeyboardInterrupt:
+                print("\nSe ha interrumpido la ejecución.")
+                break
+            except Exception as e:
+                print("Error:", e)
+
+        # Reiniciar posición de lectura de los archivos de usuarios y claves
+        intentos = 0
     else:
-        print("Usuario o contraseña incorrectos")
+        print("Login o clave incorrectos. Intente nuevamente.")
         intentos += 1
 else:
-    print("Ha excedido el número de intentos permitidos. El programa finalizara.")
+    print("Ha excedido el número de intentos permitidos. El programa terminará.")
